c0ee81e60efeb42af59bc715beb91b3d38af5279
Rishabh Bakshi <rishabh.bakshi@clarivate.com>
2022-11-30 16:19:22 +0530

    Add lazi loading concept in this application

--- routing-practice-project/src/App.js
+++ routing-practice-project/src/App.js
@@ -1,34 +1,45 @@
-import { Redirect, Route, Switch } from 'react-router-dom'
-import Layout from './components/layout/Layout'
-import AllQuote from './pages/AllQuotes'
-import NewQuote from './pages/NewQuote'
-import NotFound from './pages/NotFound'
-import QuoteDetail from './pages/QuoteDetail'
+import React, { Suspense } from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import LoadingSpinner from './components/UI/LoadingSpinner';
 
-function App () {
+const NewQuote = React.lazy(() => import('./pages/NewQuote'));
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
+const AllQuotes = React.lazy(() => import('./pages/AllQuotes'));
+
+function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <Redirect to='/quotes'>
-            <AllQuote></AllQuote>
-          </Redirect>
-        </Route>
-        <Route path='/quotes' exact>
-          <AllQuote></AllQuote>
-        </Route>
-        <Route path='/quotes/:quoteId'>
-          <QuoteDetail></QuoteDetail>
-        </Route>
-        <Route path='/new-quote'>
-          <NewQuote></NewQuote>
-        </Route>
-        <Route path='*'>
-          <NotFound></NotFound>
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner></LoadingSpinner>
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes">
+              <AllQuotes></AllQuotes>
+            </Redirect>
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes></AllQuotes>
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail></QuoteDetail>
+          </Route>
+          <Route path="/new-quote">
+            <NewQuote></NewQuote>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
-  )
+  );
 }
 
-export default App
+export default App;
