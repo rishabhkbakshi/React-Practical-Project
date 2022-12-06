2aa1a88156aa6c901b3a71c10cc9238d3c16e557
Rishabh Bakshi <rishabh.bakshi@clarivate.com>
2022-12-06 18:45:57 +0530

    add form validation in the quote form

--- routing-practice-project/src/components/quotes/QuoteForm.js
+++ routing-practice-project/src/components/quotes/QuoteForm.js
@@ -1,39 +1,99 @@
-import { Fragment, useRef, useState } from 'react'
-import { Prompt } from 'react-router-dom'
+import { Fragment, useRef, useState } from 'react';
+import { Prompt } from 'react-router-dom';
 
-import Card from '../UI/Card'
-import LoadingSpinner from '../UI/LoadingSpinner'
-import classes from './QuoteForm.module.css'
+import Card from '../UI/Card';
+import LoadingSpinner from '../UI/LoadingSpinner';
+import classes from './QuoteForm.module.css';
 
-const QuoteForm = props => {
-  const [isEntering, setIsEntering] = useState(false)
-  const authorInputRef = useRef()
-  const textInputRef = useRef()
+const QuoteForm = (props) => {
+  const [isEntering, setIsEntering] = useState(false);
+  const authorInputRef = useRef();
+  const textInputRef = useRef();
 
-  function submitFormHandler (event) {
-    event.preventDefault()
+  const [userInput, setUserInput] = useState({
+    enteredAuthor: '',
+    enteredText: '',
+  });
 
-    const enteredAuthor = authorInputRef.current.value
-    const enteredText = textInputRef.current.value
+  const [errorDisableStatus, setErrorDisableStatus] = useState({
+    error: {
+      authorField: false,
+      textField: false,
+    },
+  });
 
-    // optional: Could validate here
+  const onAuthorChangeHandler = () => {
+    if (authorInputRef.current.value !== '') {
+      setErrorDisableStatus({
+        error: {
+          authorField: false,
+          textField: textInputRef.current.value === '' ? true : false,
+        },
+      });
+    } else {
+      setErrorDisableStatus({
+        error: {
+          authorField: true,
+          textField: textInputRef.current.value === '' ? true : false,
+        },
+      });
+    }
+    setUserInput((prevState) => {
+      return { ...prevState, enteredAuthor: authorInputRef.current.value };
+    });
+  };
 
-    props.onAddQuote({ author: enteredAuthor, text: enteredText })
+  const onTextChangeHandler = () => {
+    if (textInputRef.current.value !== '') {
+      setErrorDisableStatus({
+        error: {
+          authorField: authorInputRef.current.value === '' ? true : false,
+          textField: false,
+        },
+      });
+    } else {
+      setErrorDisableStatus({
+        error: {
+          textField: true,
+          authorField: textInputRef.current.value === '' ? true : false,
+        },
+      });
+    }
+    setUserInput((prevState) => {
+      return { ...prevState, enteredText: textInputRef.current.value };
+    });
+  };
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredAuthor = authorInputRef.current.value;
+    const enteredText = textInputRef.current.value;
+
+    if (enteredAuthor === '' && enteredText === '') {
+      setErrorDisableStatus({
+        error: { authorField: true, textField: true },
+      });
+    }
+
+    if (enteredAuthor !== '' && enteredText !== '') {
+      props.onAddQuote({ author: enteredAuthor, text: enteredText });
+    }
   }
 
   const formFocusedHandler = () => {
-    setIsEntering(true)
-  }
+    setIsEntering(true);
+  };
 
   const finishEnteringHandler = () => {
-    setIsEntering(false)
-  }
+    setIsEntering(false);
+  };
 
   return (
     <Fragment>
       <Prompt
         when={isEntering}
-        message={location =>
+        message={(location) =>
           'Are you sure you want to Leave? All your entered data will be lost!'
         }
       ></Prompt>
@@ -50,22 +110,40 @@ const QuoteForm = props => {
           )}
 
           <div className={classes.control}>
-            <label htmlFor='author'>Author</label>
-            <input type='text' id='author' ref={authorInputRef} />
+            <label htmlFor="author">Author</label>
+            <input
+              type="text"
+              id="author"
+              value={userInput.enteredAuthor}
+              ref={authorInputRef}
+              onChange={onAuthorChangeHandler}
+            />
+            {errorDisableStatus.error.authorField && (
+              <p className={classes.error}>Author field can't be blank</p>
+            )}
           </div>
           <div className={classes.control}>
-            <label htmlFor='text'>Text</label>
-            <textarea id='text' rows='5' ref={textInputRef}></textarea>
+            <label htmlFor="text">Text</label>
+            <textarea
+              id="text"
+              rows="5"
+              value={userInput.enteredText}
+              ref={textInputRef}
+              onChange={onTextChangeHandler}
+            ></textarea>{' '}
+            {errorDisableStatus.error.textField && (
+              <p className={classes.error}>Text field can't be blank</p>
+            )}
           </div>
           <div className={classes.actions}>
-            <button onClick={finishEnteringHandler} className='btn'>
+            <button onClick={finishEnteringHandler} className="btn">
               Add Quote
             </button>
           </div>
         </form>
       </Card>
     </Fragment>
-  )
-}
+  );
+};
 
-export default QuoteForm
+export default QuoteForm;
